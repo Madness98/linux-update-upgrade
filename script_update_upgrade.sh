bb8d432e6b1bdd816936c24bb88253d5b5ab745a
root <kevin.gbn98@gmail.com>
2022-04-18 12:32:39 +0200

    Sending the script

---
+++ script_update_upgrade.sh
@@ -0,0 +1,6 @@
+# Script content : "script_update_upgrade.sh"
+#!/bin/bash
+echo ">>------------------------------------------------$(date)---------------------------------------------<<" >> /var/log/update_upgrade.log
+echo ">>------------------errors------------------------------$(date)---------------errors------------------------------<<" >> /var/log/update_upgrade.err
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get upgrade -y >> /var/log/update_upgrade.log 2>> /var/log/update_upgrade.err
